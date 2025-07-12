f4182614f739430db764b55ed54b839e40dc2d0e
Julian Koripamo <jkoripamo@gmail.com>
2025-07-12 19:12:42 +0100

    header texts

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <div>
+            <h1 className="font-light text-2xl sm:text-3xl text-center">Reliable, efficient delivery</h1>
+            <h2 className="font-bold text-2xl sm:text-3xl text-center mt-3">Powered by Technology</h2>
+            <p className="text-gray-500 text-center mt-5 max-w-lg">Our Artificial Intelligence powered tools use millions of project data points to ensure that your project is successful</p>
+        </div>
+    )
+}
+
+export default Header;
