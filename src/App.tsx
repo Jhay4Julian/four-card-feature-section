a1fc24fb70a66e6bbd4f0c0fcf8bc3abeac19549
Julian Koripamo <jkoripamo@gmail.com>
2025-07-12 19:12:58 +0100

    app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import './App.css'
+import Header from './components/Header'
 import './index.css'
+import FeatureSection from './components/FeatureSection';
 
 function App() {
 
   return (
     <>
-      
+      <div className='bg-gray-50 flex flex-col items-center p-10'>
+        <Header />
+        <FeatureSection/>
+      </div>
     </>
   )
 }
