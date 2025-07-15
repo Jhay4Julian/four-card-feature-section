af0e1654fd4c67488f8e8b8c925ef39a9767d8a0
Julian Koripamo <jkoripamo@gmail.com>
2025-07-15 22:49:47 +0100

    dynamic rendering

--- src/components/FeatureSection.tsx
+++ src/components/FeatureSection.tsx
@@ -1,39 +1,51 @@
 import FeatureCard from "./FeatureCard";
-import supervisorIcon from '../assets/images/icon-supervisor.svg';
-import teamIcon from '../assets/images/icon-team-builder.svg';
-import karmaIcon from '../assets/images/icon-karma.svg';
-import calculatorIcon from '../assets/images/icon-calculator.svg';
+
+const feature = [
+    {
+        title: 'Supervisor',
+        description: 'Monitors activity to identify project roadblocks',
+        icon: '/src/assets/images/icon-supervisor.svg',
+        color: 'bg-[#44c2c0]',
+    },
+    {
+        title: 'Team Builder',
+        description: 'Scans our talent network to create the optimal team for your project',
+        icon: '/src/assets/images/icon-team-builder.svg',
+        color: 'bg-[#f2545b]'
+    },
+    {
+        title: 'Karma',
+        description: 'Regularly evaluates our talent to ensure quality',
+        icon: '/src/assets/images/icon-karma.svg',
+        color: 'bg-[#fcae4f]'
+    },
+    {
+        title: 'Calculator',
+        description: 'Uses data from past projects to provide better delivery estimates',
+        icon: '/src/assets/images/icon-calculator.svg',
+        color: 'bg-[#529ffb]'
+    }
+
+]
 
 const FeatureSection = () => {
     return (
         <section>
             <div className="flex flex-col sm:flex-row items-center justify-center mt-10 gap-7">
                 <FeatureCard
-                title='Supervisor'
-                description='Monitors activity to identify project roadblocks'
-                icon={supervisorIcon}
-                color='bg-[#44c2c0]'
-            />
-            <div className="flex flex-col gap-7">
+                    {...feature[0]}
+                />
+                <div className="flex flex-col gap-7">
+                    <FeatureCard
+                        {...feature[1]}
+                    />
+                    <FeatureCard
+                        {...feature[2]}
+                    />
+                </div>
                 <FeatureCard
-                title='Team Builder'
-                description='Scans our talent network to create the optimal team for your project'
-                icon={teamIcon}
-                color='bg-[#f2545b]'
-            />
-            <FeatureCard
-                title='Karma'
-                description='Regularly evaluates our talent to ensure quality'
-                icon={karmaIcon}
-                color='bg-[#fcae4f]'
-            />
-            </div>
-            <FeatureCard
-                title='Calculator'
-                description='Uses data from past projects to provide better delivery estimates'
-                icon={calculatorIcon}
-                color='bg-[#529ffb]'
-            />
+                    {...feature[3]}
+                />
             </div>
         </section>
     )
